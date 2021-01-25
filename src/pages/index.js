a200278924197d067d2a2a44ec51229f9b3e4902
John Milazzo <github@johnmilazzo.com>
2021-01-25 16:34:15 -0600

    checkin

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,15 @@
 import Head from 'next/head'
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div>
+    <div className="mx-auto md:max-w-3xl md:px-4 min-h-screen">
       <Head>
         <title>Next.js 101</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="min-h-screen flex flex-col justify-center">
+      <main className="min-h-screen flex flex-col justify-center bg-white p-4">
         <img
           className="m-auto my-4 w-24 sm:w-auto"
           alt="Ihatetomatoes"
@@ -17,6 +18,7 @@ export default function Home() {
         <h1 className="mx-auto text-4xl font-bold">
           Welcome to Next.js 101!
         </h1>
+        <Link href="/page-two"><a>Page Two</a></Link>
       </main>
 
     </div>
---
+++ src/pages/page-two.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+export default function PageTwo() {
+  return (
+    <>
+    <h1>Page Two</h1>
+
+    <Link href="/"><a>Go Home</a></Link>
+    </>
+  )
+};
